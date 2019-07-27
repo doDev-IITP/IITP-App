9b165e5fa73a4df5e5e425f7f28c92d65b8faef6
Aman Jee <amangrobo@gmail.com>
2019-07-28 00:20:13 +0530

    added mess routes and mess fragment changes

--- app/src/main/java/com/grobo/notifications/network/MessRoutes.java
+++ app/src/main/java/com/grobo/notifications/network/MessRoutes.java
@@ -1,7 +1,9 @@
 package com.grobo.notifications.network;
 
+import okhttp3.RequestBody;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.PATCH;
 import retrofit2.http.Path;
@@ -12,6 +14,6 @@ public interface MessRoutes {
     @GET("/mess/{studentMongoId}")
     Call<ResponseBody> getMessData(@Path("studentMongoId") String studentMongoId);
     @PATCH("/mess/cancel/{studentMongoId}")
-    Call<ResponseBody> cancelMeal(@Path("studentMongoId") String studentMongoId);
+    Call<ResponseBody> cancelMeal(@Path("studentMongoId") String studentMongoId, @Body RequestBody body);
 
 }
