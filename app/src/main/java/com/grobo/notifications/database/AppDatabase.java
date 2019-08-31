4b9fd2fa07b3b08324bd78c527f2ab070e1d3459
Aman Jee <amangrobo@gmail.com>
2019-08-31 17:58:07 +0530

    Update AppDatabase.java

--- app/src/main/java/com/grobo/notifications/database/AppDatabase.java
+++ app/src/main/java/com/grobo/notifications/database/AppDatabase.java
@@ -6,6 +6,8 @@ import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 import androidx.room.TypeConverters;
+import androidx.room.migration.Migration;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 import com.grobo.notifications.admin.clubevents.ClubEventDao;
 import com.grobo.notifications.admin.clubevents.ClubEventItem;
@@ -19,16 +21,20 @@ import com.grobo.notifications.notifications.NotificationDao;
 import com.grobo.notifications.todolist.Goal;
 import com.grobo.notifications.todolist.TodoDao;
 
-@Database(entities = {Notification.class, FeedItem.class, ClubItem.class, ClubEventItem.class, Goal.class}, version = 2, exportSchema = false)
+@Database(entities = {Notification.class, FeedItem.class, ClubItem.class, ClubEventItem.class, Goal.class}, version = 3, exportSchema = false)
 @TypeConverters(Converters.class)
 public abstract class AppDatabase extends RoomDatabase {
 
     private static volatile AppDatabase INSTANCE;
 
     public abstract NotificationDao notificationDao();
+
     public abstract FeedDao feedDao();
+
     public abstract ClubDao clubDao();
+
     public abstract ClubEventDao eventDao();
+
     public abstract TodoDao todoDao();
 
     public static AppDatabase getDatabase(Context context) {
@@ -37,12 +43,20 @@ public abstract class AppDatabase extends RoomDatabase {
                 if (INSTANCE == null) {
                     INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
                             AppDatabase.class, "app_database")
+                            .addMigrations(MIGRATION_2_3)
                             .allowMainThreadQueries()
-                            .fallbackToDestructiveMigration()
+                            .fallbackToDestructiveMigrationFrom(1)
                             .build();
                 }
             }
         }
         return INSTANCE;
     }
+
+    private static final Migration MIGRATION_2_3 = new Migration(2, 3) {
+        @Override
+        public void migrate(SupportSQLiteDatabase database) {
+            database.execSQL("CREATE TABLE `todo` (`id` INTEGER PRIMARY KEY AUTOINCREMENT,  `name` TEXT, `isChecked` INTEGER, `dateAdded` INTEGER)");
+        }
+    };
 }
